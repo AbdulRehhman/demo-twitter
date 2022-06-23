96cee011d4f99bc0c197f461f9bbb35a855138aa
= <=>
2022-06-24 02:24:12 +0500

    Adding Relationship Table

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :tweeets
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
 end
